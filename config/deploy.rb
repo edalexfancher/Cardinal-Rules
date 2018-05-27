01364abcbdbd21e983d61ebadac768bdded12cde
Phant0mnyc <edmlounge@gmail.com>
2018-05-26 20:18:52 -0400

    Reverted

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-set :rvm_ruby_version, '2.5.1'
+
 
 ## Defaults:
 # set :scm,           :git
